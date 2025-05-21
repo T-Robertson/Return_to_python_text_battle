ef921c226480cb43ebdb1761c5d9de3e10d59d5e
Thomas Robertson <91926056+T-Robertson@users.noreply.github.com>
2025-05-21 21:13:48 +1000

    code clean up

--- classes/inventory.py
+++ classes/inventory.py
@@ -5,3 +5,12 @@ class Item:
         self.description = description
         self.prop = prop
 
+
+    # Create some Items
+potion = Item("Potion", "potion", "Heals 50 HP", 50)
+hipotion = Item("Hi-Potion", "potion", "Heals 100 HP", 100)
+superpotion = Item("Super Potion", "potion", "Heals 1000 HP", 1000)
+elixer = Item("Elixer", "elixer", "Fully restores HP/MP of one party member", 9999)
+hielixer = Item("MegaElixer", "elixer", "Fully restores party's HP/MP", 9999)
+
+grenade = Item("Grenade", "attack", "Deals 500 damage", 500)
--- classes/magic.py
+++ classes/magic.py
@@ -13,3 +13,14 @@ class Spell:
         return random.randrange(low, high)
 
 
+# Create Black Magic
+fire = Spell("Fire", 25, 600, "black")
+thunder = Spell("Thunder", 25, 600, "black")
+blizzard = Spell("Blizzard", 25, 600, "black")
+meteor = Spell("Meteor", 40, 1200, "black")
+quake = Spell("Quake", 14, 140, "black")
+
+# Create White Magic
+cure = Spell("Cure", 25, 620, "white")
+cura = Spell("Cura", 32, 1500, "white")
+curaga = Spell("Curaga", 50, 6000, "white")
